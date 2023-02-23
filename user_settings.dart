290cd5b700c3cd4c37a15d31589444f0bec372cb
josejasona <55518382+josejasona@users.noreply.github.com>
2023-02-23 08:56:18 -0500

    Added files for the HomeScreen + Nav
    Added all files needed for the HomeScreen and the the NavigationBar. The home.dart file is where the assessment should be linked to, while the main_page displays what the "home" should display. I did not include the main file as I think we should use Saif's for now. Also included my widget for large text on the main_page

---
+++ user_settings.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'progress.dart';
+
+class userSettings extends StatelessWidget {
+  const userSettings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Text("User Settings")
+    );
+  }
+}
