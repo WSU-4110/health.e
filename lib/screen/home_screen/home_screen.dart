925d6a81ce1022f818ef9f35c0d102321fda84e8
mshul <fv2223@wayne.edu>
2023-03-18 18:36:32 -0400

    email authentication

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -1,38 +1,79 @@
-
 import 'package:flutter/material.dart';
 import 'package:healthe/screen/home_screen/user_settings.dart';
 import 'package:healthe/screen/home_screen/workouts.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:healthe/auth.dart';
 import 'MainPage.dart';
 import 'progress.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
+   HomeScreen({Key? key}) : super(key: key);
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+  
 
-  List pages =
-  [
+class _HomeScreenState extends State<HomeScreen> {
+   final User? user = Auth().currentUser;
+
+  Future<void> signOut() async {
+  await Auth().signOut();
+}
+
+  Widget _title() {
+  return const Text('Firebase Auth');
+}
+
+  Widget _userid() {
+  return Text(user?.email ?? 'User email');
+}
+
+  Widget _signOutButton() {
+  return ElevatedButton(
+    onPressed: signOut,
+    child: const Text('Sign Out'),
+  );
+} 
+
+  late final List pages = [
     const MainPage(),
     const Workouts(),
     const Progress(),
-    const userSettings()
+    const userSettings(),
+    Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _userid(),
+            _signOutButton(),
+          ],
+        ),
+      ),
   ];
   int currentIndex = 0;
-  void onTap(int index){
+  void onTap(int index) {
     setState(() {
       currentIndex = index;
     });
   }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[currentIndex],
+      appBar: AppBar(
+        title: _title(),
+      ),
+      body:
+      
+      
+       
+      pages[currentIndex],
+      
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.blue,
@@ -44,13 +85,15 @@ class _HomeScreenState extends State<HomeScreen> {
         showSelectedLabels: false,
         elevation: 0,
         items: [
-          const BottomNavigationBarItem(label:"Home" , icon: Icon(Icons.home)),
-          const BottomNavigationBarItem(label:"Workouts" , icon: Icon(Icons.fitness_center)),
-          const BottomNavigationBarItem(label:"Progress" , icon: Icon(Icons.bar_chart)),
-          const BottomNavigationBarItem(label:"Profile" , icon: Icon(Icons.person)),
+          const BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          const BottomNavigationBarItem(
+              label: "Workouts", icon: Icon(Icons.fitness_center)),
+          const BottomNavigationBarItem(
+              label: "Progress", icon: Icon(Icons.bar_chart)),
+          const BottomNavigationBarItem(
+              label: "Profile", icon: Icon(Icons.person)),
         ],
       ),
     );
   }
 }
-
