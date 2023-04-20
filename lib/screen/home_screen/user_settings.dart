95efd5f48bfc22c23b66cde25a2fccb0a8084e59
mshul <fv2223@wayne.edu>
2023-04-20 13:13:09 -0400

    add_workout

--- lib/screen/home_screen/user_settings.dart
+++ lib/screen/home_screen/user_settings.dart
@@ -2,45 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserSettings extends StatelessWidget {
-final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-Future<void> _deleteAccount() async {
-try {
-await _firebaseAuth.currentUser!.delete();
-} catch (e) {
-// Handle any errors here
-print('Error deleting account: $e');
-}
-}
+  Future<void> _deleteAccount(BuildContext context) async {
+    bool deleteConfirmed = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Are you sure?"),
+          content: Text("This action will permanently delete your account."),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            TextButton(
+              child: Text("Delete"),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+    if (deleteConfirmed == true) {
+      try {
+        await _firebaseAuth.currentUser!.delete();
+        Navigator.pushReplacementNamed(context, '/login');
+      } catch (e) {
+        // Handle any errors here
+        print('Error deleting account: $e');
+      }
+    }
+  }
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('User Settings'),
-),
-body: Center(
-child: Column(
-mainAxisAlignment: MainAxisAlignment.center,
-children: [
-ElevatedButton(
-onPressed: () async {
-await _deleteAccount();
-Navigator.pushReplacementNamed(context, '/login');
-},
-child: Text('Delete Account'),
-),
-SizedBox(height: 16),
-ElevatedButton(
-onPressed: () async {
-await _firebaseAuth.signOut();
-Navigator.pushReplacementNamed(context, '/login');
-},
-child: Text('Sign out'),
-),
-],
-),
-),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Settings'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                await _deleteAccount(context);
+              },
+              child: Text('Delete Account'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                await _firebaseAuth.signOut();
+                Navigator.pushReplacementNamed(context, '/login');
+              },
+              child: Text('Sign out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/screen/widgets/add_workout.dart
+++ lib/screen/widgets/add_workout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class AddWorkoutScreen extends StatefulWidget {
   const AddWorkoutScreen({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class AddWorkoutScreen extends StatefulWidget {
 
 class _AddWorkoutScreenState extends State<AddWorkoutScreen> {
   final _workoutTextController = TextEditingController();
+  DateTime _selectedDateTime = DateTime.now();
 
   @override
   void dispose() {
@@ -32,16 +34,33 @@ class _AddWorkoutScreenState extends State<AddWorkoutScreen> {
     }
 
     final now = DateTime.now();
-    final date = DateTime(now.year, now.month, now.day);
+    if (_selectedDateTime.isBefore(now)) {
+      // The selected date and time is before the current date and time,
+      // so we don't add the workout.
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text(
+              'The selected date and time cannot be before the current date and time.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
     final workoutData = {
-      'log_date': Timestamp.fromDate(date),
+      'log_date': Timestamp.fromDate(_selectedDateTime),
       'username': user.displayName ?? user.email,
       'workout_log': workout,
     };
 
-    await FirebaseFirestore.instance
-        .collection('workout_log')
-        .add(workoutData);
+    await FirebaseFirestore.instance.collection('workout_log').add(workoutData);
 
     Navigator.pop(context);
   }
@@ -65,7 +84,59 @@ class _AddWorkoutScreenState extends State<AddWorkoutScreen> {
             ),
             SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: _addWorkout,
+              onPressed: () async {
+                final now = DateTime.now();
+                final selectedDate = await showDatePicker(
+                  context: context,
+                  initialDate: now,
+                  firstDate: now,
+                  lastDate: DateTime(now.year + 1),
+                );
+                if (selectedDate == null) {
+                  return;
+                }
+                final selectedTime = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.fromDateTime(now),
+                );
+                if (selectedTime == null) {
+                  return;
+                }
+                final selectedDateTime = DateTime(
+                    selectedDate.year,
+                    selectedDate.month,
+                    selectedDate.day,
+                    selectedTime.hour,
+                    selectedTime.minute);
+                if (selectedDateTime.isBefore(now)) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Please select a future date and time'),
+                      duration: Duration(seconds: 3),
+                    ),
+                  );
+                  return;
+                }
+                final user = FirebaseAuth.instance.currentUser;
+                if (user == null) {
+                  // The user is not signed in, so we can't add a workout.
+                  return;
+                }
+                final workout = _workoutTextController.text.trim();
+                if (workout.isEmpty) {
+                  // The workout text field is empty, so we don't add anything.
+                  return;
+                }
+                final workoutData = {
+                  'log_date': Timestamp.fromDate(selectedDateTime),
+                  'username': user.displayName ?? user.email,
+                  'workout_log': workout,
+                };
+                await FirebaseFirestore.instance
+                    .collection('workout_log')
+                    .add(workoutData);
+                Navigator.pop(context);
+              },
               child: Text('Submit'),
             ),
             TextButton(
